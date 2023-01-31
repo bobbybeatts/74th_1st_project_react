aced76f69bb33db42d82d74822593965039f241f
kophellia <alseh1541@naver.com>
2023-01-31 20:11:11 +0900

    템플릿 표시 에러발생

--- src/erp/account/budget/api/index.js
+++ src/erp/account/budget/api/index.js
@@ -1,3 +1,11 @@
 import accountApi from 'api/accountApi';
 
 export const searchYearList = (action) => accountApi.get('/operate/parentaccountlist', {});
+export const selectBudgetStatus = (action) =>
+    accountApi.get('/budget/budgetstatus', {
+    params: {
+        accountPeriodNo: action.params.accountPeriodNo,
+        deptCode: action.params.deptCode,
+        workplaceCode: action.params.workplaceCode
+        }
+    });
